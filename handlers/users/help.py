921798f4f67703c71c8543700127967d6d43c1b4
frogy9cat <ramzeek_voker@mail.ru>
2022-08-10 01:43:08 +0500

    inline_yt

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -2,8 +2,8 @@ from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandHelp
 from utils.pic_for_help import get_info_pic
 from loader import dp
-
+from keyboards.inline import my_channel
 
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
-    await message.answer_photo(photo=get_info_pic(), caption="Инлайн бот для поиска, возвращающий ссылку на видео в ютуб.")
+    await message.answer_photo(photo=get_info_pic(), caption="Инлайн бот для поиска, возвращающий ссылку на видео в ютуб.", reply_markup=my_channel.Channel)
---
+++ keyboards/inline/my_channel.py
@@ -0,0 +1,9 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+Channel = InlineKeyboardMarkup(
+inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Канал разработчика", url="https://t.me/python_b_end"),
+    ],
+    ],
+)
