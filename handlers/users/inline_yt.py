b4afab3284759ef97a33e101236c3532e21adbc9
frogy9cat <ramzeek_voker@mail.ru>
2022-08-11 10:42:39 +0500

    vid_1.0

--- handlers/users/inline_yt.py
+++ handlers/users/inline_yt.py
@@ -1,6 +1,10 @@
 from aiogram.types import InlineQuery, InputMediaVideo, InlineQueryResultArticle, InputTextMessageContent
+from aiogram import types
 from loader import dp
 from utils.yt_vid_get import video_from_yt
+from aiogram.dispatcher.filters.builtin import Text
+from io import BytesIO
+from pytube import YouTube
 
 
 @dp.inline_handler()
@@ -20,3 +24,20 @@ async def inline_video_search(inline_query: InlineQuery):
             items.append(item)
     if items:
         await inline_query.answer(results=items, cache_time=1)
+
+
+
+@dp.message_handler(Text(startswith="http")) 
+async def get_video(message:types.Message):
+    await message.answer("Видео загружается...")
+    link=message.text  
+    buffer=BytesIO() 
+    url=YouTube (link) 
+    if url.check_availability() is None:
+        video=url.streams.get_highest_resolution()
+        video.stream_to_buffer(buffer=buffer) 
+        buffer.seek(0) 
+        filename=url.title
+        await message.reply_video(video=buffer, caption=f"{filename}\n@python_b_end") 
+    else:
+        await message.answer("Неверный формат ссылки.")
