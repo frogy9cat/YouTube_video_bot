708809f0a14ea86c3376a0faf66c99ee769335e9
frogy9cat <ramzeek_voker@mail.ru>
2022-08-12 23:25:36 +0500

    first

--- data/config.py
+++ data/config.py
@@ -4,7 +4,3 @@ import os
 BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot token
 ADMINS = list(os.environ.get("ADMINS"))  # adminlar ro'yxati
 IP = str(os.environ.get("ip"))  # Xosting ip manzili
-
-# ADMINS=['486178287']
-# BOT_TOKEN='5389210352:AAG5wIuNTE4EreKdsPrLNrDv_uVjdF6jBoQ'
-# ip='localhost'
