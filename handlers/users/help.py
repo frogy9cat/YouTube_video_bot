bbcd6ad7aed625f5fa9ea7271c00cd7264ea8915
frogy9cat <ramzeek_voker@mail.ru>
2022-08-18 01:35:10 +0500

    commit

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -6,4 +6,4 @@ from keyboards.inline import my_channel
 
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
-    await message.answer_photo(photo=get_info_pic(), caption="Инлайн бот для поиска, возвращающий ссылку на видео в ютуб.", reply_markup=my_channel.Channel)
+    await message.answer_photo(photo=get_info_pic(), caption="Инлайн бот для поиска видео из YouTube. Также бот способен скачивать видео оттуда если отправить ему ссылку. Но видео скачается если оно небольшого размера.\n\nСоздатель: @frogy_cat\nБлагодарю за использование моего бота!)", reply_markup=my_channel.Channel)
